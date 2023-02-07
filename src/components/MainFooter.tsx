83821fbd6e6febd18358f6255234f5ef27a63d90
Raki <killer.vp.mic@gmail.com>
2023-02-07 18:41:40 -0300

    added social icons

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,5 +1,6 @@
 import { createStyles, Container, Group, ActionIcon } from '@mantine/core';
-import { TbBrandTwitter, TbBrandYoutube, TbBrandInstagram } from 'react-icons/tb';
+import { TbBrandTwitter, TbBrandYoutube, TbBrandInstagram, TbBrandGmail } from 'react-icons/tb';
+import { AiOutlineLinkedin, AiFillGithub } from 'react-icons/ai';
 import { FC } from 'react';
 import MainLogo from './MainLogo';
 
@@ -9,9 +10,8 @@ const ICON_SIZE = 20;
 const useStyles = createStyles((theme) => ({
   footer: {
     marginTop: 120,
-    borderTop: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
-    }`,
+    borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+      }`,
   },
 
   inner: {
@@ -39,16 +39,24 @@ const MainFooter: FC = () => {
   return (
     <div className={classes.footer}>
       <Container className={classes.inner}>
-        <MainLogo height={28} width={200} color='white'/>
+        <MainLogo height={28} width={200} color='white' />
         <Group spacing={20} className={classes.links} position="right" noWrap>
-          <ActionIcon size="lg">
-            <TbBrandTwitter size={ICON_SIZE}  />
+          <ActionIcon size="lg" component='a' href='https://github.com/ademondev' target='_blank'>
+            <AiFillGithub
+              size={ICON_SIZE}
+            />
+          </ActionIcon>
+          <ActionIcon size="lg" component='a' href='https://www.linkedin.com/in/alejandro-pedemonte-806131265/' target='_blank'>
+            <AiOutlineLinkedin
+              size={ICON_SIZE}
+
+            />
           </ActionIcon>
           <ActionIcon size="lg">
-            <TbBrandYoutube size={ICON_SIZE}  />
-          </ActionIcon>
-          <ActionIcon size="lg">
-            <TbBrandInstagram size={ICON_SIZE}  />
+            <TbBrandGmail
+              size={ICON_SIZE}
+              onClick={() => window.location.href = 'mailto:ademondev@gmail.com'}
+            />
           </ActionIcon>
         </Group>
       </Container>
